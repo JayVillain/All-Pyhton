756766b28903ddd23276ca134c9feb8c3bf3797e
JayVillain <saipi3825@gmail.com>
2025-09-03 11:20:22 +0000

    Sep 3, 2025, 6:20 PM

--- latihan_python_pemula/calcu.py
+++ latihan_python_pemula/calcu.py
@@ -1,1 +1,10 @@
 print("INI CALCULATOR TAMBAH-TAMABAHAN")
+try:
+    angka1 = int(input("mauskan angka ke 1 : "))
+    angka2 = int(input("masukan angka ke 2 : ")) 
+    hasil = angka1 + angka2
+    print("hasil", hasil)
+except:
+    print("ini wajib angka bos kalau masukin huruf eror")
+
+print("itu hasilnya bousss")
