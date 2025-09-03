f2bb32346e888f8e0be777b4530037d6281a6d38
JayVillain <saipi3825@gmail.com>
2025-09-03 11:13:44 +0000

    Sep 3, 2025, 6:13 PM

--- variabel.py
+++ variabel.py
@@ -1,8 +1,8 @@
 print("INI KALKULATOR PENGURANGAN YA ")
 
 try:
-    angka1 = int(input("masukan angka yang ke 1"))
-    angka2 = int(input("masukan angka yang ke 2"))
+    angka1 = int(input("masukan angka yang ke 1 :"))
+    angka2 = int(input("masukan angka yang ke 2 :"))
     hasil = angka1 - angka2
     print("hasil", hasil)
 except:
