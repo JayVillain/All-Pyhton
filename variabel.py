0896fabde6766a15a93b464603bc60ad2110c439
JayVillain <saipi3825@gmail.com>
2025-09-03 11:13:11 +0000

    Sep 3, 2025, 6:13 PM

--- variabel.py
+++ variabel.py
@@ -4,5 +4,7 @@ try:
     angka1 = int(input("masukan angka yang ke 1"))
     angka2 = int(input("masukan angka yang ke 2"))
     hasil = angka1 - angka2
+    print("hasil", hasil)
 except:
-    print()
+    print("kalau masukin huruf eror bos wajib angka ye")
+print("itu hasilnya bos")
