21cedbb2d9159113301c1beb6f61aa38b54b95c5
JayVillain <saipi3825@gmail.com>
2025-09-03 11:09:35 +0000

    Sep 3, 2025, 6:09 PM

--- variabel.py
+++ variabel.py
@@ -1,3 +1,10 @@
-nama = "budi"
-tinggi = 172
-berat = 55
+print ("==KALULATOR GACOR==")
+
+try:
+    angka1 = int(input("angka1: "))
+    angka2 = int(input("angka2: "))
+    hasil = angka1 + angka2
+    print("hasil",hasil)
+except :
+    print("INI LU SALAH KOMTOL")
+print("done bang")
