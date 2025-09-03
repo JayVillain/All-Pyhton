3c82191eb91dea28046316b0ff9732786eeb4479
JayVillain <saipi3825@gmail.com>
2025-09-03 11:12:17 +0000

    Sep 3, 2025, 6:12 PM

--- variabel.py
+++ variabel.py
@@ -1,10 +1,8 @@
-print ("==KALULATOR GACOR==")
+print("INI KALKULATOR PENGURANGAN YA ")
 
 try:
-    angka1 = int(input("angka1: "))
-    angka2 = int(input("angka2: "))
-    hasil = angka1 + angka2
-    print("hasil",hasil)
-except :
-    print("INI LU SALAH KOMTOL")
-print("done bang")
+    angka1 = int(input("masukan angka yang ke 1"))
+    angka2 = int(input("masukan angka yang ke 2"))
+    hasil = angka1 - angka2
+except:
+    print()
