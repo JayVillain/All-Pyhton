8678bbde364d8978d30b3c493982ef9df397b827
JayVillain <saipi3825@gmail.com>
2025-09-03 11:14:16 +0000

    Sep 3, 2025, 6:14 PM

--- variabel.py
+++ variabel.py
@@ -1,9 +1,9 @@
-print("INI KALKULATOR PENGURANGAN YA ")
+print("INI KALKULATOR PENAMBAHAN YA ")
 
 try:
     angka1 = int(input("masukan angka yang ke 1 :"))
     angka2 = int(input("masukan angka yang ke 2 :"))
-    hasil = angka1 - angka2
+    hasil = angka1 + angka2
     print("hasil", hasil)
 except:
     print("kalau masukin huruf eror bos wajib angka ye")
