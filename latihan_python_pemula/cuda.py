ef80218b19d9b58acb189980ff928c63aecffd3b
JayVillain <saipi3825@gmail.com>
2025-09-04 16:46:04 +0000

    Sep 4, 2025, 11:46 PM

---
+++ latihan_python_pemula/cuda.py
@@ -0,0 +1,9 @@
+print("calcu minusss")
+
+try:
+    angka1 = int(input("masukan angkka ke 1 dahulun bos : "))
+    angka2 = int(input("masukan angka ke 2 bos : "))
+    hasil = angka1 - angka2
+    print("hasil", hasil)
+except:
+    print("ini hasilnya bous")
