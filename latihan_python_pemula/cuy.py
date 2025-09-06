8e2136d30fe0b161f8d3ce8c7c9e254e0fa2bce8
JayVillain <saipi3825@gmail.com>
2025-09-06 15:25:27 +0000

    Sep 6, 2025, 10:25 PM

--- latihan_python_pemula/cuy.py
+++ latihan_python_pemula/cuy.py
@@ -1,8 +1,1 @@
-print ("kiki +")
-try:
-    angka1 = int(input("masukan angka bos :"))
-    angka2 = int(input("masukan angka lagi bos :"))
-    hasil = angka1 + angka2
-    print("hasi;", hasil)
-except:
-    print("ini isinya yang bener bos bos")
+nama = "alex bizer"
