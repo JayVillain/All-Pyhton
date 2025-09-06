877fb4559886ebae76e4c0c20f761dda0286aada
JayVillain <saipi3825@gmail.com>
2025-09-06 15:23:36 +0000

    Sep 6, 2025, 10:23 PM

--- latihan_python_pemula/cuy.py
+++ latihan_python_pemula/cuy.py
@@ -2,3 +2,7 @@ print ("kiki +")
 try:
     angka1 = int(input("masukan angka bos :"))
     angka2 = int(input("masukan angka lagi bos :"))
+    hasil = angka1 + angka2
+    print("hasi;", hasil)
+except:
+    print("ini hasilnya bos")
